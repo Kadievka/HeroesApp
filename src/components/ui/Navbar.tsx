73e2d1290cefede26045e90b45f081b4e4cfb82a
Kadievka Salcedo <kadieka.s.ks@gmail.com>
2022-05-17 22:41:38 +0200

    176. Navigate push / replace - useNavigate

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
 
+  const navigate = useNavigate();
+
   const handleLogout = () => {
-    console.log("TODO handleLogout")
+    navigate("/login", {
+      replace: true
+    });
   }
 
   return (
