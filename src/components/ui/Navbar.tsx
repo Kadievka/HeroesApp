543ace8ad47de24d8b3c55e570ab172c10ee46ae
Kadievka Salcedo <kadieka.s.ks@gmail.com>
2022-05-17 22:08:10 +0200

    174. Colocar clase de la ruta activa

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
+
+  const handleLogout = () => {
+    console.log("TODO handleLogout")
+  }
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
 
@@ -21,11 +26,19 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <NavLink className="nav-item nav-link" to="/login">
+
+          <span className="nav-item nav-link text-info">
+            Kadievka
+          </span>
+
+          <button
+            className="nav-item nav-link btn"
+            onClick={handleLogout}
+          >
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
