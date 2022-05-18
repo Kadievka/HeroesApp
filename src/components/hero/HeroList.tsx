d1c15420979c3bb69c8d89f05481448efb36884b
Kadievka Salcedo <kadieka.s.ks@gmail.com>
2022-05-18 22:47:05 +0200

    181. Nota: useMemo

--- src/components/hero/HeroList.tsx
+++ src/components/hero/HeroList.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useMemo } from "react";
 import getHeroesByPublisher from "../../helpers/utils/getHeroesByPublisher";
 import Hero from "../../interfaces/Hero";
 import HeroListPropsInterface from "../../interfaces/HeroListPropsInterface";
 import HeroCard from "./HeroCard";
 
 const HeroList = ({ publisher }: HeroListPropsInterface) => {
-  const heroes: Hero[] = getHeroesByPublisher(publisher);
+  const heroes: Hero[] = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
     <div className="card-columns">
